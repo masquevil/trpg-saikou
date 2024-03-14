01612ef4bc4e4b15f0a4c01ac1fe682ee2ce0ecc
masquevil <masquevil@gmail.com>
2024-03-14 18:36:43 +0800

    [shell] update shell

--- shell/copy.sh
+++ shell/copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 复制 local/*.example.ts 到 local/*.ts
 
-for file in `ls src/local/*.example.ts`
+for file in src/local/*.example.ts
 do
-  cp $file `echo $file|sed  's/.example.ts/.ts/'`
+  cp "$file" "${file//.example.ts/.1.ts}"
 done
