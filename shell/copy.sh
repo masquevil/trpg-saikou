4686927952a61867ebafc8f8e9721110dc5e2f8e
masquevil <masquevil@gmail.com>
2024-03-17 02:05:13 +0800

    [shell] bug fix

--- shell/copy.sh
+++ shell/copy.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 # 复制 local/*.example.ts 到 local/*.ts
 
 for file in src/local/*.example.ts
 do
-  cp "$file" "${file//.example.ts/.ts}"
+  new_file=$(echo "$file" | sed 's/\.example\.ts/\.ts/')
+  cp "$file" "$new_file"
 done
