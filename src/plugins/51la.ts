6ee6d09ba750c644fd26f1442994d887dea710d5
masquevil <masquevil@gmail.com>
2024-04-07 11:53:39 +0800

    [coc-card] 更新群二维码

--- src/plugins/51la.ts
+++ src/plugins/51la.ts
@@ -2,8 +2,32 @@ import { LAEventID, FeatureNames } from '@/constants/coc-card/51la';
 export { LAEventID, FeatureNames };
 
 interface ParamsOfFeature {
-  [key: string]: string | number | boolean;
   name: FeatureNames;
+  /**
+   * 自动年龄修正是否成功。
+   *   条件: MORE_AGE
+   */
+  success?: boolean;
+  /**
+   * 保存的文件名，front | back。
+   *   条件: CA_SAVE_DOWNLOAD, CA_SAVE_REGEN
+   */
+  file?: string;
+  /**
+   * 成功率显示模式，full | simple。
+   *   条件: MORE_TOTAL_MODE
+   */
+  mode?: 'full' | 'simple';
+  /**
+   * 使用的预设职业
+   *   条件: PANE_USE_JOB, PAPER_USE_JOB
+   */
+  job?: string;
+  /**
+   * 使用的预设武器
+   *   条件: PANE_USE_WEAPON, PAPER_USE_WEAPON
+   */
+  weapon?: string;
 }
 
 declare global {
