1eb1e7a5bcead7a02f62e4368198e15e41aea0eb
masquevil <masquevil@gmail.com>
2024-08-24 12:12:28 +0800

    [kp-ads] fix stupid ts-check

--- src/apps/kp-ads/models/story.ts
+++ src/apps/kp-ads/models/story.ts
@@ -1,32 +1,33 @@
-import type { Story } from '../types/story';
+import type { Story, StoryInfoOverrides } from '../types/story';
 import { storyInfoOverrides } from '../tables/storyInfoOverrides';
 import { dicechoStoryInfos } from '../tables/dicechoStory';
 
 export default class StoryModel {
   static getStoriesByKeyOrTitle(list: [string?, string?][]): Story[] {
-    return list
-      .map((item) => {
-        const [idMatcher, titleMatcher] = item;
-        const storyOverride = storyInfoOverrides.find((info) => {
-          return info.id === idMatcher || info.title === titleMatcher;
-        });
-        if (!storyOverride) return;
+    const storyOverrides: StoryInfoOverrides[] = [];
+    list.forEach((item) => {
+      const [idMatcher, titleMatcher] = item;
+      const storyOverride = storyInfoOverrides.find((info) => {
+        return info.id === idMatcher || info.title === titleMatcher;
+      });
+      if (storyOverride) storyOverrides.push(storyOverride);
+    });
 
-        const { id, coverUrl, playerNumber = [4, 6], ...rest } = storyOverride;
-        const dicechoStory = dicechoStoryInfos.find((info) => info._id === id);
-        const { cnmodsAliaseId, rateAvg, tags, validRateCount } = dicechoStory || {};
-        return {
-          id,
-          coverUrl: coverUrl || dicechoStory?.coverUrl || '',
-          playerNumber,
-          ...rest,
-          isDicecho: !id.startsWith('sox-id-'),
-          cnmodsAliaseId,
-          rateAvg,
-          tags,
-          validRateCount,
-        };
-      })
-      .filter((story) => !!story);
+    return storyOverrides.map((storyOverride) => {
+      const { id, coverUrl, playerNumber = [4, 6], ...rest } = storyOverride;
+      const dicechoStory = dicechoStoryInfos.find((info) => info._id === id);
+      const { cnmodsAliaseId, rateAvg, tags, validRateCount } = dicechoStory || {};
+      return {
+        id,
+        coverUrl: coverUrl || dicechoStory?.coverUrl || '',
+        playerNumber,
+        ...rest,
+        isDicecho: !id.startsWith('sox-id-'),
+        cnmodsAliaseId,
+        rateAvg,
+        tags,
+        validRateCount,
+      };
+    });
   }
 }
