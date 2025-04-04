28190318a93ab5cb193c42b314eaa5480e4eb1b2
masquevil <masquevil@users.noreply.github.com>
2025-04-04 09:52:01 +0000

    deploy: bd705a410779bca54cb408ef1bac998faca088d7

---
+++ assets/AppView-XKAS9ATE.js
@@ -0,0 +1,1 @@
+import{R as e}from"./RollTool-Du9_aIcB.js";import{d as o,c as t,k as a,o as s}from"./index-C0LUEXIf.js";import{_ as p}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./random-D7cEhbNa.js";const r={class:"page"},c=o({__name:"AppView",setup(_){return(n,m)=>(s(),t("main",r,[a(e)]))}}),u=p(c,[["__scopeId","data-v-e922e64a"]]);export{u as default};
