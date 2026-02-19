c40808f61d2b1cbc4a97893f47e2f5974005e9ed
侠小然 <masquevil@gmail.com>
2026-02-19 15:05:40 +0800

    [scenario-editor] 重构md解析器，方便未来拓展

--- src/apps/scenario-editor/hooks/useScenarioParser/index.ts
+++ src/apps/scenario-editor/hooks/useScenarioParser/index.ts
@@ -1,32 +1,30 @@
 import { ref } from 'vue';
-import { Marked, Renderer } from 'marked';
+import { Marked } from 'marked';
 import NpcDataManager from '../../models/npcDataManager';
 import createNpcBlockExtensions, { NpcBlockRenderers } from './npc-block/extensions';
 
 export type CustomRenderers = NpcBlockRenderers;
 
-// 自定义渲染器
-const renderer = new Renderer();
-
-// 重写 heading 渲染器
-renderer.heading = function ({ tokens, depth }) {
-  const text = this.parser.parseInline(tokens);
-  const className = `md-h${depth}`;
-  return `<h${depth} id="${text.replace(/\s+/g, '-')}" class="${className}">${text}</h${depth}>`;
-};
-
-// 重写 paragraph 渲染器
-renderer.paragraph = function ({ tokens }) {
-  const text = this.parser.parseInline(tokens);
-  return `<p class="md-p">${text}</p>`;
-};
-
-// 创建 Marked 实例并注册扩展
 const marked = new Marked({
-  renderer,
   breaks: true,
   gfm: true,
 });
+
+// common extensions
+marked.use({
+  renderer: {
+    heading({ tokens, depth }) {
+      const text = this.parser.parseInline(tokens);
+      return `<h${depth} id="${text.replace(/\s+/g, '-')}" class="md-h${depth}">${text}</h${depth}>`;
+    },
+    paragraph({ tokens }) {
+      const text = this.parser.parseInline(tokens);
+      return `<p class="md-p">${text}</p>`;
+    },
+  },
+});
+
+let initialized = false;
 
 export default function useScenarioParser(
   moduleName: string,
@@ -34,7 +32,11 @@ export default function useScenarioParser(
   renderers: CustomRenderers,
 ) {
   const npcManager = ref(new NpcDataManager(moduleName));
-  marked.use(createNpcBlockExtensions({ npcManager }, renderers));
+  if (!initialized) {
+    initialized = true;
+    // npc block extensions
+    marked.use(createNpcBlockExtensions({ npcManager }, renderers));
+  }
 
   return {
     parse: async () => marked.parse(content),
